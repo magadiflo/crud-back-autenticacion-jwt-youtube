1248f6ce9f1d085115b4a37c516868454f0b8c63
Martín <magadiflo@gmail.com>
2022-05-18 12:33:50 -0500

    Implementación de las entidades Rol, Usuario, UsuarioPrincipal y RolNombre

--- src/main/java/com/tutorial/crud/security/entity/Rol.java
+++ src/main/java/com/tutorial/crud/security/entity/Rol.java
@@ -1,4 +1,44 @@
 package com.tutorial.crud.security.entity;
 
+import com.tutorial.crud.security.enums.RolNombre;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "roles")
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "rol_nombre")
+    private RolNombre rolNombre;
+
+    public Rol() {
+
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
 }
