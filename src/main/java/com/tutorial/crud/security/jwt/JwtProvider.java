5090e2301e2f49a036da3c2fe155a09eceb885c9
Martín <magadiflo@gmail.com>
2022-05-18 19:01:39 -0500

    Capítulo 4: Cración de las clases relacionadas a la generación y validación del TOKEN

--- src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
@@ -1,4 +1,61 @@
 package com.tutorial.crud.security.jwt;
 
+import com.tutorial.crud.security.entity.UsuarioPrincipal;
+import io.jsonwebtoken.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * Genera el token. Tiene métodos para validar
+ * que esté bien formado el token, que no esté
+ * expirado, etc.
+ */
+@Component
 public class JwtProvider {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtProvider.class);
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private int expiration;
+
+    public String generateToken(Authentication authentication) {
+        UsuarioPrincipal usuarioPrincipal = (UsuarioPrincipal) authentication.getPrincipal();
+        return Jwts.builder()
+                .setSubject(usuarioPrincipal.getUsername())
+                .setIssuedAt(new Date()) //Fecha de creación
+                .setExpiration(new Date(new Date().getTime() + this.expiration * 1000))
+                .signWith(SignatureAlgorithm.HS512, this.secret)
+                .compact();
+    }
+
+    public String getNombreUsuarioFromToken(String token){
+        return Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(this.secret).parseClaimsJws(token);
+            return true;
+        } catch (MalformedJwtException e){
+            logger.error("Token mal formado");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Token no soportado");
+        } catch (ExpiredJwtException e) {
+            logger.error("Token expirado");
+        } catch (IllegalArgumentException e) {
+            logger.error("Token vacío");
+        } catch (SignatureException e) {
+            logger.error("Fail en la firma");
+        }
+        return false;
+    }
+
 }
--- src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtTokenFilter.java
@@ -1,4 +1,60 @@
 package com.tutorial.crud.security.jwt;
 
-public class JwtTokenFilter {
+import com.tutorial.crud.security.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Se ejecuta una vez por cada petición, comprueba
+ * que sea válido el token (utilizando el JwtProvider)
+ * Si es válido, permite el acceso al recurso, caso contrario
+ * lanza la excepción
+ */
+
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtTokenFilter.class);
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String token = this.getToken(request);
+            if(token != null && this.jwtProvider.validateToken(token)){
+                String nombreUsuario = jwtProvider.getNombreUsuarioFromToken(token);
+                UserDetails userDetails = this.userDetailsService.loadUserByUsername(nombreUsuario);
+                UsernamePasswordAuthenticationToken auth =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        }catch (Exception e){
+            logger.error("Fail en el método doFilterInternal()");
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if(header != null && header.startsWith("Bearer")){
+            return header.replace("Bearer ", "");
+        }
+        return null;
+    }
 }
