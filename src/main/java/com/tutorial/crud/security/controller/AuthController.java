db8fcbc973819832f1b60491beeae0f0da777f1d
Martín <magadiflo@gmail.com>
2022-05-23 13:47:06 -0500

    Implementación del Refresh Token

--- src/main/java/com/tutorial/crud/security/controller/AuthController.java
+++ src/main/java/com/tutorial/crud/security/controller/AuthController.java
@@ -22,6 +22,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.ParseException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -82,7 +83,11 @@ public class AuthController {
         return new ResponseEntity(jwtDto, HttpStatus.OK);
     }
 
-
-
+    @PostMapping("/refresh")
+    public ResponseEntity<JwtDto> refresh(@RequestBody JwtDto jwtDto) throws ParseException {
+        String token = this.jwtProvider.refreshToken(jwtDto);
+        JwtDto jwt = new JwtDto(token);
+        return new ResponseEntity<JwtDto>(jwt, HttpStatus.OK);
+    }
 
 }
--- src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
+++ src/main/java/com/tutorial/crud/security/jwt/JwtProvider.java
@@ -1,5 +1,9 @@
 package com.tutorial.crud.security.jwt;
 
+import com.nimbusds.jwt.JWT;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.JWTParser;
+import com.tutorial.crud.security.dto.JwtDto;
 import com.tutorial.crud.security.entity.UsuarioPrincipal;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
@@ -9,6 +13,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,7 +41,7 @@ public class JwtProvider {
                 .setSubject(usuarioPrincipal.getUsername())
                 .claim("roles", roles)
                 .setIssuedAt(new Date()) //Fecha de creación
-                .setExpiration(new Date(new Date().getTime() + this.expiration * 1000))
+                .setExpiration(new Date(new Date().getTime() + this.expiration))
                 .signWith(SignatureAlgorithm.HS512, this.secret.getBytes())
                 .compact();
     }
@@ -61,6 +66,21 @@ public class JwtProvider {
             logger.error("Fail en la firma");
         }
         return false;
+    }
+
+    public String refreshToken(JwtDto jwtDto) throws ParseException {
+        JWT jwt = JWTParser.parse(jwtDto.getToken());
+        JWTClaimsSet claims = jwt.getJWTClaimsSet();
+        String nombreUsuario  = claims.getSubject();
+        List<String> roles = (List<String>) claims.getClaim("roles");
+
+        return Jwts.builder()
+                .setSubject(nombreUsuario)
+                .claim("roles", roles)
+                .setIssuedAt(new Date()) //Fecha de creación
+                .setExpiration(new Date(new Date().getTime() + this.expiration))
+                .signWith(SignatureAlgorithm.HS512, this.secret.getBytes())
+                .compact();
     }
 
 }
