c1c1bc5537e001116c31739eafc35869082ad089
Martín <magadiflo@gmail.com>
2022-05-24 11:57:19 -0500

    Recibiendo el DTO del usuario por petición y obteniendo datos del usuario para poblar el correo y guardar token password en la BD

--- src/main/java/com/tutorial/crud/emailpassword/controller/EmailController.java
+++ src/main/java/com/tutorial/crud/emailpassword/controller/EmailController.java
@@ -3,12 +3,15 @@ import com.tutorial.crud.dto.Mensaje;
 import com.tutorial.crud.emailpassword.dto.EmailValuesDTO;
 import com.tutorial.crud.emailpassword.service.EmailService;
 
+import com.tutorial.crud.security.entity.Usuario;
+import com.tutorial.crud.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,18 +22,43 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
+    @Autowired
+    private UsuarioService usuarioService;
+
     @Value("${spring.mail.username}")
     private String mailFrom;
 
+    @Value("${mail.subject}")
+    private String subject;
+
     @PostMapping("/send-email")
     public ResponseEntity<?> sendEmailTemplate(@RequestBody EmailValuesDTO dto){
+
+        Optional<Usuario> usuarioOptional = this.usuarioService.getByNombreUsuarioOrEmail(dto.getMailTo());
+
+        if(!usuarioOptional.isPresent()){
+            return new ResponseEntity<>(new Mensaje("No existe ningún usuario con esas credenciales"), HttpStatus.NOT_FOUND);
+        }
+
+        Usuario usuario = usuarioOptional.get();
         dto.setMailFrom(this.mailFrom);
-        dto.setSubject("Cambio de contraseña");
-        dto.setUsername("Martín");
-        UUID uuid = UUID.randomUUID();
-        String tokenPassword = uuid.toString();
+        dto.setMailTo(usuario.getEmail());
+        dto.setUsername(usuario.getNombreUsuario());
+        dto.setSubject(this.subject);
+
+        String tokenPassword = this.getTokenPassword();
+
         dto.setTokenPassword(tokenPassword);
+        usuario.setTokenPassword(tokenPassword);
+
+        this.usuarioService.save(usuario);
         this.emailService.sendEmail(dto);
-        return new ResponseEntity(new Mensaje("Te hemos enviado un correo"), HttpStatus.OK);
+
+        return new ResponseEntity(new Mensaje("Te hemos enviado un correo. Por favor revísalo."), HttpStatus.OK);
+    }
+
+    private String getTokenPassword() {
+        UUID uuid = UUID.randomUUID();
+        return uuid.toString();
     }
 }
